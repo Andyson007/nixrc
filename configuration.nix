560626177d3f5c2989cc73a6a0c101c1ebd768fa
Andyson007 <andreas.jan.van.der.meulen@gmail.com>
2024-06-11 22:17:44 +0200

    enabled aso

--- configuration.nix
+++ configuration.nix
@@ -30,9 +30,10 @@
   # Enable sound.
   # hardware.pulseaudio.enable = true;
   # OR
-  sound.enable = true;
   services.pipewire = {
     enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
     pulse.enable = true;
   };
 
