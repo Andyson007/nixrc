00c40aee47ffa9b21d5555dff1d339983b96a387
Andyson007 <andreas.jan.van.der.meulen@gmail.com>
2024-06-13 12:16:01 +0200

    removed ssh

--- configuration.nix
+++ configuration.nix
@@ -83,11 +83,11 @@
   # Enable the OpenSSH daemon.
   services.openssh = {
     enable = true;
-    settings.PasswordAuthentication = true;
+    # settings.PasswordAuthentication = true;
   };
 
   # Open ports in the firewall.
-  networking.firewall.allowedTCPPorts = [ 22 ];
+  # networking.firewall.allowedTCPPorts = [ 22 ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
