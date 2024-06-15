47f455a92f812fde3e8e1b36b0a2b2c9e82b89c6
Andyson007 <andreas.jan.van.der.meulen@gmail.com>
2024-06-15 22:01:30 +0200

    made computer go to hibernate after an hour of suspend-then-hibernate

--- configuration.nix
+++ configuration.nix
@@ -22,6 +22,10 @@
 
   time.timeZone = "Europe/Oslo";
 
+  systemd.sleep.extraConfig = ''
+  HibernateDelaySec=3600s
+  '';
+
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
