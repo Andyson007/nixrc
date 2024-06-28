78f77c106d7089ea73b6fffe43c18262fd66d5e9
Andyson007 <andreas.jan.van.der.meulen@gmail.com>
2024-06-28 10:09:36 +0200

    added haskell correctly

--- users/andy.nix
+++ users/andy.nix
@@ -90,7 +90,8 @@
     taplo
     # sql-language-server
     nodePackages_latest.bash-language-server
-    dhall-lsp-server
+    haskellPackages.ghcide
+
   ]) ++ (with nixpkgs-unstable; [
     neovim
   ]);
