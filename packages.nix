b42e0d30d2639067d12df61a14070aa71b873230
Andyson007 <andreas.jan.van.der.meulen@gmail.com>
2024-08-18 12:55:49 +0200

    changed the config file to not commit changes made while building the system

--- packages.nix
+++ packages.nix
@@ -16,13 +16,15 @@
     (pkgs.writeShellScriptBin "rebuild" ''
       pushd /home/andy/.config/.nixrc/
       ${pkgs.git}/bin/git diff
+      ${pkgs.git}/bin/git add .
       echo "Write a commit message"
       read commit_message
       sudo nixos-rebuild switch --flake .
       if [[ $? -eq 0 ]]; then
-        ${pkgs.git}/bin/git add .
         ${pkgs.git}/bin/git commit -m "$commit_message"
         ${pkgs.git}/bin/git push origin main
+      else
+        git reset
       fi
       popd
     '')
