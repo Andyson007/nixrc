0e57b707e2cd7ac06972cebadc1f597edadd8682
Andyson007 <andreas.jan.van.der.meulen@gmail.com>
2024-08-19 19:25:36 +0200

    [kanshi/hyprland] moved monitor config to kanshi

--- users/andy/kanshi.nix
+++ users/andy/kanshi.nix
@@ -30,19 +30,26 @@
               status = "enable";
             }
           ];
-          exec = "${pkgs.hyprland}/bin/hyprctl dispatch --batch \"\\
-keyword workspace=1,monitor:eDP-1,                                  on-created-empty:alacritty\\
-keyword workspace=2,monitor:eDP-1,                                  on-created-empty:alacritty\\
-keyword workspace=3,monitor:desc:Dell Inc. DELL U2410 F525M27UD0DL\\
-keyword workspace=4,monitor:desc:Dell Inc. DELL U2410 F525M27UD0DL\\
-keyword workspace=5,monitor:desc:Dell Inc. DELL U2410 F525M29HC43L, on-created-empty: firefox\\
-keyword workspace=6,monitor:desc:Dell Inc. DELL U2410 F525M27UD0DL, on-created-empty: obsidian\\
-keyword workspace=7,monitor:desc:Dell Inc. DELL U2410 F525M27UD0DL, on-created-empty: spotify\\
-keyword workspace=8,monitor:eDP-1,                                  on-created-empty:(cd ~/vaults/Knowledge/;neovide .)\\
-keyword workspace=9,monitor:desc:Dell Inc. DELL U2410 F525M27UD0DL, on-created-empty: discord\\
-keyword workspace=10,monitor:eDP-1\\
-\"
-";
+          exec = pkgs.lib.strings.concatStrings [
+            "${pkgs.hyprland}/bin/hyprctl --batch dispatch"
+            (
+              pkgs.lib.strings.concatStrings
+              (
+                pkgs.lib.strings.intersperse ";" [
+  "keyword workspace= 1,monitor:eDP-1,                                  on-created-empty:alacritty"
+  "keyword workspace= 2,monitor:eDP-1,                                  on-created-empty:alacritty"
+  "keyword workspace= 3,monitor:desc:Dell Inc. DELL U2410 F525M27UD0DL"
+  "keyword workspace= 4,monitor:desc:Dell Inc. DELL U2410 F525M27UD0DL"
+  "keyword workspace= 5,monitor:desc:Dell Inc. DELL U2410 F525M29HC43L, on-created-empty: firefox"
+  "keyword workspace= 6,monitor:desc:Dell Inc. DELL U2410 F525M27UD0DL, on-created-empty: obsidian"
+  "keyword workspace= 7,monitor:desc:Dell Inc. DELL U2410 F525M27UD0DL, on-created-empty: spotify"
+  "keyword workspace= 8,monitor:eDP-1,                                  on-created-empty:(cd ~/vaults/Knowledge/;neovide .)"
+  "keyword workspace= 9,monitor:desc:Dell Inc. DELL U2410 F525M27UD0DL, on-created-empty: discord"
+  "keyword workspace=10,monitor:eDP-1"
+                ]
+              )
+            )
+          ];
         };
       }
       { 
@@ -55,19 +62,26 @@ keyword workspace=10,monitor:eDP-1\\
               status = "enable";
             }
           ];
-          exec = "${pkgs.hyprland}/bin/hyprctl dispatch --batch \"\\
-keyword workspace=1,monitor:eDP-1,                                  on-created-empty:alacritty\\
-keyword workspace=2,monitor:eDP-1,                                  on-created-empty:alacritty\\
-keyword workspace=3,monitor:eDP-1\\
-keyword workspace=4,monitor:eDP-1\\
-keyword workspace=5,monitor:eDP-1, on-created-empty: firefox\\
-keyword workspace=6,monitor:eDP-1, on-created-empty: obsidian\\
-keyword workspace=7,monitor:eDP-1, on-created-empty: spotify\\
-keyword workspace=8,monitor:eDP-1,                                  on-created-empty:(cd ~/vaults/Knowledge/;neovide .)\\
-keyword workspace=9,monitor:eDP-1, on-created-empty: discord\\
-keyword workspace=10,monitor:eDP-1\\
-\"
-";
+          exec = pkgs.lib.strings.concatStrings [
+            "${pkgs.hyprland}/bin/hyprctl --batch dispatch"
+            (
+              pkgs.lib.strings.concatStrings
+              (
+                pkgs.lib.strings.intersperse ";" [
+  "keyword workspace= 1,monitor:eDP-1, on-created-empty:alacritty"
+  "keyword workspace= 2,monitor:eDP-1, on-created-empty:alacritty"
+  "keyword workspace= 3,monitor:eDP-1"
+  "keyword workspace= 4,monitor:eDP-1"
+  "keyword workspace= 5,monitor:eDP-1"
+  "keyword workspace= 6,monitor:eDP-1, on-created-empty: obsidian"
+  "keyword workspace= 7,monitor:eDP-1, on-created-empty: spotify"
+  "keyword workspace= 8,monitor:eDP-1, on-created-empty:(cd ~/vaults/Knowledge/;neovide .)"
+  "keyword workspace= 9,monitor:eDP-1, on-created-empty: discord"
+  "keyword workspace=10,monitor:eDP-1"
+                ]
+              )
+            )
+          ];
         };
       }
     ];
