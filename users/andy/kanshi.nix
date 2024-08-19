0b49c06b4eee6b4b4c1c3c1e25cb0ceca997cb17
Andyson007 <andreas.jan.van.der.meulen@gmail.com>
2024-08-19 21:05:25 +0200

    [kanshi] minor changed neovide spawn command

--- users/andy/kanshi.nix
+++ users/andy/kanshi.nix
@@ -36,14 +36,14 @@
               pkgs.lib.strings.concatStrings
               (
                 pkgs.lib.strings.intersperse ";" [
-  "keyword workspace= 1,monitor:eDP-1,                                  on-created-empty:alacritty"
-  "keyword workspace= 2,monitor:eDP-1,                                  on-created-empty:alacritty"
+  "keyword workspace= 1,monitor:eDP-1,                                  on-created-empty: alacritty"
+  "keyword workspace= 2,monitor:eDP-1,                                  on-created-empty: alacritty"
   "keyword workspace= 3,monitor:desc:Dell Inc. DELL U2410 F525M27UD0DL"
   "keyword workspace= 4,monitor:desc:Dell Inc. DELL U2410 F525M27UD0DL"
   "keyword workspace= 5,monitor:desc:Dell Inc. DELL U2410 F525M29HC43L, on-created-empty: firefox"
   "keyword workspace= 6,monitor:desc:Dell Inc. DELL U2410 F525M27UD0DL, on-created-empty: obsidian"
   "keyword workspace= 7,monitor:desc:Dell Inc. DELL U2410 F525M27UD0DL, on-created-empty: spotify"
-  "keyword workspace= 8,monitor:eDP-1,                                  on-created-empty:(cd ~/vaults/Knowledge/;neovide .)"
+  "keyword workspace= 8,monitor:eDP-1,                                  on-created-empty: neovide -- --cmd 'cd ~/vaults/Knowledge/"
   "keyword workspace= 9,monitor:desc:Dell Inc. DELL U2410 F525M27UD0DL, on-created-empty: discord"
   "keyword workspace=10,monitor:eDP-1"
                 ]
@@ -68,14 +68,14 @@
               pkgs.lib.strings.concatStrings
               (
                 pkgs.lib.strings.intersperse ";" [
-  "keyword workspace= 1,monitor:eDP-1, on-created-empty:alacritty"
-  "keyword workspace= 2,monitor:eDP-1, on-created-empty:alacritty"
+  "keyword workspace= 1,monitor:eDP-1, on-created-empty: alacritty"
+  "keyword workspace= 2,monitor:eDP-1, on-created-empty: alacritty"
   "keyword workspace= 3,monitor:eDP-1"
   "keyword workspace= 4,monitor:eDP-1"
   "keyword workspace= 5,monitor:eDP-1"
   "keyword workspace= 6,monitor:eDP-1, on-created-empty: obsidian"
   "keyword workspace= 7,monitor:eDP-1, on-created-empty: spotify"
-  "keyword workspace= 8,monitor:eDP-1, on-created-empty:(cd ~/vaults/Knowledge/;neovide .)"
+  "keyword workspace= 8,monitor:eDP-1, on-created-empty: neovide -- --cmd 'cd ~/vaults/Knowledge/"
   "keyword workspace= 9,monitor:eDP-1, on-created-empty: discord"
   "keyword workspace=10,monitor:eDP-1"
                 ]
