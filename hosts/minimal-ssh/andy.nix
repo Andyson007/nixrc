9b3de0bdbab6a971bda182da8be2b03499c8e040
Andyson007 <andreas.jan.van.der.meulen@gmail.com>
2024-08-23 18:13:19 +0200

    [host:minimal-ssh] added a user

---
+++ hosts/minimal-ssh/andy.nix
@@ -0,0 +1,38 @@
+{
+  config,
+  pkgs,
+  nixpkgs-unstable,
+  ...
+}: {
+  imports = [
+  ];
+
+  home = {
+    username = "andy";
+    homeDirectory = "/home/andy";
+
+    stateVersion = "24.05"; # DO NOT CHANGE
+
+    shellAliases = {
+        a = "ls --color -klAhs";
+        l = "ls --color -klhs";
+    };
+  };
+
+  programs = {
+    git = {
+      enable = true;
+      userName = "Andyson007";
+      userEmail = "andreas.jan.van.der.meulen@gmail.com";
+      extraConfig = {
+        merge.conflictStyle = "diff3";
+        diff.colorMoved = "default";
+        alias = {
+          staash = "stash --all";
+        };
+      };
+    };
+
+    home-manager.enable = true;
+  };
+}
