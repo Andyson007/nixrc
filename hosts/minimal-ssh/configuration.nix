9e98794f9e1083c40278153bcee3ece8d10383f8
Andyson007 <andreas.jan.van.der.meulen@gmail.com>
2024-08-23 18:10:00 +0200

    enabled ssh stuff

--- hosts/minimal-ssh/configuration.nix
+++ hosts/minimal-ssh/configuration.nix
@@ -1,15 +1,13 @@
-# Edit this configuration file to define what should be installed on
-# your system. Help is available in the configuration.nix(5) man page, on
-# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-
-{ config, lib, pkgs, nixpkgs-unstable, inputs, ... }:
-
 {
-  imports =
-    [
-      ../../hardware-configuration.nix
-      ./packages.nix
-    ];
+  pkgs,
+  inputs,
+  builtins,
+  ...
+}: {
+  imports = [
+    ../../hardware-configuration.nix
+    ./packages.nix
+  ];
 
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
@@ -19,83 +17,44 @@
 
   networking = {
     hostName = "andyco";
-    networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+    networkmanager.enable = true;
     wireless.enable = false;
   };
 
   time.timeZone = "Europe/Oslo";
 
   systemd.sleep.extraConfig = ''
-  HibernateDelaySec=3600s
+    HibernateDelaySec=3600s
   '';
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.andy = {
-     isNormalUser = true;
-     extraGroups = [ "wheel" "networkmanager" ]; # Enable ‘sudo’ for the user.
+    isNormalUser = true;
+    extraGroups = ["wheel" "networkmanager"]; # Enable ‘sudo’ for the user.
+    openssh.authorizedKeys.keys = [
+      builtins.readFile
+      ./keys/id_ed25519.pub
+    ];
   };
 
   environment.sessionVariables = {
     XDG_CONFIG_HOME = "$HOME/.config";
   };
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+  services.openssh = {
+    enable = true;
+  };
 
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh = {
-  #   enable = false;
-  # };
-
-  # Open ports in the firewall.
-  networking.firewall.allowedTCPPorts = [ 22 ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This option defines the first version of NixOS you have installed on this particular machine,
-  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-  #
-  # Most users should NEVER change this value after the initial install, for any reason,
-  # even if you've upgraded your system to a new NixOS release.
-  #
-  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
-  # to actually do that.
-  #
-  # This value being lower than the current NixOS release does NOT mean your system is
-  # out of date, out of support, or vulnerable.
-  #
-  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
-  # and migrated your data accordingly.
-  #
+  networking.firewall.allowedTCPPorts = [22];
   # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
   system.stateVersion = "24.05"; # Did you read the comment?
 
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings.experimental-features = ["nix-command" "flakes"];
 
   programs.nix-ld = {
     enable = true;
     libraries = with pkgs; [
-        zlib
-        libgcc
+      zlib
+      libgcc
     ];
   };
 
@@ -112,8 +71,4 @@
     dates = "02:00";
     randomizedDelaySec = "45min";
   };
-
-  fonts.packages = with pkgs; [
-    (nerdfonts.override { fonts = [ "0xProto" ]; })
-  ];
 }
--- hosts/minimal-ssh/packages.nix
+++ hosts/minimal-ssh/packages.nix
@@ -1,22 +1,24 @@
-{ pkgs, nixpkgs-unstable, ... }:
 {
-  environment.systemPackages = (with pkgs; [
+  pkgs,
+  ...
+}: {
+  environment.systemPackages = with pkgs; [
     stdenv
     git
-    (pkgs.writeShellScriptBin "rebuild" ''
+    (writeShellScriptBin "rebuild" ''
       pushd /home/andy/.nixrc/
-      ${pkgs.git}/bin/git diff
-      ${pkgs.git}/bin/git add .
+      ${git}/bin/git diff
+      ${git}/bin/git add .
       echo "Write a commit message"
       read commit_message
       sudo nixos-rebuild switch --flake .
       if [[ $? -eq 0 ]]; then
-        ${pkgs.git}/bin/git commit -m "$commit_message"
-        ${pkgs.git}/bin/git push origin main
+        ${git}/bin/git commit -m "$commit_message"
+        ${git}/bin/git push origin main
       else
         git reset
       fi
       popd
     '')
-  ]);
+  ];
 }
