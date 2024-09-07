20e84ababf570c1d3902c4f197b9ff0405d82c36
Andyson007 <andreas.jan.van.der.meulen@gmail.com>
2024-09-07 07:41:44 +0200

    limited the amount of nix derivations to 10

--- hosts/dual-school/configuration.nix
+++ hosts/dual-school/configuration.nix
@@ -1,6 +1,3 @@
-# Edit this configuration file to define what should be installed on
-# your system. Help is available in the configuration.nix(5) man page, on
-# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 {
   config,
   lib,
@@ -17,7 +14,10 @@
   ];
 
   # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot = {
+    enable = true;
+    configurationLimit = 10;
+  };
   boot.loader.efi.canTouchEfiVariables = true;
 
   nixpkgs.config.allowUnfree = true;
