057ac1ac6472481b8675f76e01175ba156431b26
Andyson007 <andreas.jan.van.der.meulen@gmail.com>
2024-09-12 14:10:47 +0200

    removed opengl

--- hosts/dual-school/configuration.nix
+++ hosts/dual-school/configuration.nix
@@ -52,10 +52,6 @@
     pulse.enable = true;
   };
 
-  hardware = {
-    bluetooth.enable = true;
-    opengl.enable = true;
-  };
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
