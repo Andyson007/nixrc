eb2b6419c050f3db1035ada40440812211aa058c
Andyson007 <andreas.jan.van.der.meulen@gmail.com>
2025-03-01 21:03:53 +0100

    decreased the config limit

--- hosts/dual-school/configuration.nix
+++ hosts/dual-school/configuration.nix
@@ -16,7 +16,7 @@
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot = {
     enable = true;
-    configurationLimit = 10;
+    configurationLimit = 3;
   };
   boot.loader.efi.canTouchEfiVariables = true;
 
